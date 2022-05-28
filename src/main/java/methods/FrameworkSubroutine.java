4835180c2b26685bcc51721dcd2fa22bb268817b
Sayem <sayem@mislbd.com>
2022-05-29 02:30:25 +0600

    Gbl params and utility object's final parameter declared.

--- src/main/java/methods/FrameworkSubroutine.java
+++ src/main/java/methods/FrameworkSubroutine.java
@@ -20,10 +20,12 @@ import static utilityClasses.ExternalFileUtilities.*;
  **/
 
 public class FrameworkSubroutine {
+    public static final String GBL_PARAMS = "GlobalParams", UTILITY = "UtilityObjects";
+
     public static Actions action;
     public static WebDriver driver;
     public static ArrayList<String> TEST_RESULT_REPORT, TEST_DETAIL_LOG, BATCH_RUN_REPORT;
-    public static Properties UTILITY_OBJECTS, CURRENT_GBL_PARAM, COMMON_OBJECTS;
+    public static Properties UTILITY_OBJECTS, CURRENT_GBL_PARAM;
     public static Map<String, Map<String, String>> array = new HashMap<>();
 
     public static String JAR_FILE_PATH, CURRENT_DATE_TIME, LOGIN_PAGE_TAB_NAME, HOME_PAGE_TAB_NAME;
@@ -36,14 +38,12 @@ public class FrameworkSubroutine {
             TEST_DETAIL_LOG = new ArrayList<>();
             TEST_RESULT_REPORT = new ArrayList<>();
             BATCH_RUN_REPORT = new ArrayList<>();
-            CURRENT_GBL_PARAM = externalObjectRepositoryGet("GlobalParams");
-            COMMON_OBJECTS = externalObjectRepositoryGet("CommonObjects");
-            UTILITY_OBJECTS = externalObjectRepositoryGet("UtilityObjects");
+            CURRENT_GBL_PARAM = externalObjectRepositoryGet(GBL_PARAMS);
+            UTILITY_OBJECTS = externalObjectRepositoryGet(UTILITY);
             //FrmSubExternalFilesLoad("GlobalParams", CURRENT_GBL_PARAM);
             SCREENSHOT_COUNT = Integer.parseInt(CURRENT_GBL_PARAM.getProperty("GblScreenShotCount").split("~")[0]);
-            LOGIN_PAGE_TAB_NAME = COMMON_OBJECTS.getProperty("GuiLoginTab").split("~")[1];
-            HOME_PAGE_TAB_NAME = COMMON_OBJECTS.getProperty("GuiAppHomePageTab").split("~")[1];
-
+            LOGIN_PAGE_TAB_NAME = CURRENT_GBL_PARAM.getProperty("GblAppLoginPageTab").split("~")[0];
+            HOME_PAGE_TAB_NAME = CURRENT_GBL_PARAM.getProperty("GblAppHomePageTab").split("~")[0];
         } catch (IOException e) {
             errorMsgArrayInsert(errorStackTracePrint(e), null);
         }
